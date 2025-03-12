ea87ffc3519f20efaa374b8a617985061acfdc1e
Nick Sillik <nick@sillik.org>
2025-03-12 06:03:00 -0400

    Bring in latest changes

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -7,7 +7,7 @@
 
 use std::thread;
 
-use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::delay::{Delay, FreeRtos};
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 
@@ -17,21 +17,22 @@ fn main() -> anyhow::Result<()> {
     let peripherals = Peripherals::take()?;
     let mut led = PinDriver::output(peripherals.pins.gpio18)?;
     thread::spawn(move || loop {
-        led.set_high().expect("5");
-        FreeRtos::delay_ms(500);
-        led.set_low().expect("6");
-        FreeRtos::delay_ms(500);
+        blink(&mut led, 443);
     });
 
     let mut led = PinDriver::output(peripherals.pins.gpio2)?;
     thread::spawn(move || loop {
-        led.set_high().expect("2");
-        FreeRtos::delay_ms(333);
-        led.set_low().expect("3");
-        FreeRtos::delay_ms(214);
+        blink(&mut led, 500);
     });
 
     loop {
         FreeRtos::delay_ms(500);
     }
+}
+
+fn blink<T: OutputPin>(led: &mut PinDriver<'_, T, Output>, delay: u32) {
+    led.set_high().expect("2");
+    FreeRtos::delay_ms(delay);
+    led.set_low().expect("3");
+    FreeRtos::delay_ms(delay);
 }
